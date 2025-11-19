c7957ea7782ad5848d168d4c320afaac6a4d19e1
Matt Pua <mattpua49@gmail.com>
2025-11-19 00:27:59 -0500

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
+import { Instrument_Sans } from 'next/font/google';
 
-const inter = Inter({
+const instrumentSans = Instrument_Sans({
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={instrumentSans.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider search={{enabled: true}}>{children}</RootProvider>
+        
+        <RootProvider search={{enabled: true}}><Analytics />{children}</RootProvider>
       </body>
     </html>
   );
