9eff22fe7fe7e0c75059149d2a06e59838b45159
Matt Pua <mattpua49@gmail.com>
2025-11-19 00:51:47 -0500

    feat: add link to github in sidebar

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,10 +1,22 @@
 import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout, type DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
 import { baseOptions } from '@/lib/layout.shared';
+import { Github } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
+  const docsOptions: DocsLayoutProps = {
+  ...baseOptions(),
+  tree: source.pageTree,
+  sidebar: {
+    banner: 
+    <div>
+      <Link aria-label='GitHub' target='_blank' className="rounded-full border inline-flex p-2 hover:bg-gray-100 items-center gap-2 transition ease-in-out duration-200 group" href="https://github.com/mattpua/steam-unofficial-docs"><Github className="group-hover:text-blue-500" size={20} /> </Link>
+    </div>
+  },
+};
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
+    <DocsLayout {...docsOptions} >
       {children}
     </DocsLayout>
   );
